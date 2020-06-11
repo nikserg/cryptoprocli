ff4792214210c87822412213e228b21aa5884792
n.zarubin <n.zarubin@itsirius.ru>
2020-06-11 17:55:37 +0300

    m

--- Exception/Cli.php
+++ Exception/Cli.php
@@ -11,4 +11,15 @@ namespace nikserg\cryptoprocli\Exception;
  */
 class Cli extends \Exception
 {
+    /**
+     * Сообщение, из которого удалены символы, ломающие json
+     *
+     *
+     * @return mixed|string
+     */
+    public function getMessageSafe()
+    {
+        $message = $this->getMessage();
+        return mb_convert_encoding($message, 'UTF-8', 'UTF-8');
+    }
 }
